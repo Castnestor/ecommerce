25938262a9cf7417fcbebd532a068ead84cfe12c
Nestor Castillo <castnestor@gmail.com>
2024-09-13 17:58:47 +1200

    adding outsorce component and more changes to scss

--- pixelhub-app/components/Outsorce.jsx
+++ pixelhub-app/components/Outsorce.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Image from "next/image";
 
 export default function Outsorce() {
   return (
-    <div>
-        {/*this is the code*/}
+    <div className="outsorce-info">
+      <div className="outsorce-image">
+        <Image
+          src="/images/home_Digital-marketing-stats-nz-1230x770.webp"
+          alt="Explanation of how much time a person uses internet"
+          fill
+        />
+      </div>
+      <div className="outcome-data">
+        <h3>Why Outsource Your Visual Content Creation?</h3>
+        <ul>
+          <li>Enhance your branding</li>
+          <li>Showcase excellence</li>
+          <li>Build consumer trust and engagement</li>
+          <li>Avoid the hiring hassle</li>
+          <li>No software purchasing</li>
+          <li>No equipment acquisition</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
--- pixelhub-app/src/app/page.js
+++ pixelhub-app/src/app/page.js
@@ -1,5 +1,6 @@
 import GraphicBanner from "../../components/GraphicBanner";
 import Hero from "../../components/Hero";
+import Outsorce from "../../components/Outsorce";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Hero/>
       <section className="section-packages">
       <GraphicBanner/>
+      <Outsorce/>
       </section>
     </div>
   );
